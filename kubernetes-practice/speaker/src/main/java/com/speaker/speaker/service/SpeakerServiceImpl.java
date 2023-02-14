ed2d153588d4b612dde577ddae022f693f6d2a7b
VDK64 <dkvoznyuk@yandex.ru>
2023-02-15 00:38:23 +0300

    Start trying to deploy on cloud.

--- kubernetes-practice/speaker/src/main/java/com/speaker/speaker/service/SpeakerServiceImpl.java
+++ kubernetes-practice/speaker/src/main/java/com/speaker/speaker/service/SpeakerServiceImpl.java
@@ -1,13 +1,12 @@
 package com.speaker.speaker.service;
 
 import com.speaker.speaker.dtos.SpeakerDto;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class SpeakerServiceImpl implements SpeakerService{
+public class SpeakerServiceImpl implements SpeakerService {
     private final RestTemplate restTemplate;
 
     public SpeakerServiceImpl(RestTemplate restTemplate) {
